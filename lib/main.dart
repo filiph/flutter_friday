a4021f80b209a7459c207e924bc81e398ccbfe5e
Filip Hracek <filip.hracek@gmail.com>
2018-08-08 11:28:23 -0700

    InheritedWidget test

--- lib/main.dart
+++ lib/main.dart
@@ -3,28 +3,63 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
 
-int fibonacci(int n) {
-  return n < 2 ? n : (fibonacci(n - 1) + fibonacci(n - 2));
-}
-
 final bigStyle = TextStyle(
   fontSize: 32.0,
   color: Colors.black87,
 );
 
-class MyApp extends StatelessWidget {
+int fibonacci(int n) {
+  return n < 2 ? n : (fibonacci(n - 1) + fibonacci(n - 2));
+}
+
+class InheritedColor extends InheritedWidget {
+  final Color color;
+
+  InheritedColor({
+    Key key,
+    @required this.color,
+    Widget child,
+  }) : super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(InheritedColor oldWidget) => oldWidget.color != color;
+
+  static InheritedColor of(BuildContext context) =>
+      context.inheritFromWidgetOfExactType(InheritedColor);
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() {
+    return new MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: new ThemeData(
-          primarySwatch: Colors.deepOrange,
-        ),
-        home: Scaffold(
+    return InheritedColor(
+      color: Colors.deepOrange,
+      child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: new ThemeData(
+            primarySwatch: Colors.deepOrange,
+          ),
+          home: Scaffold(
             appBar: AppBar(
               title: Text('OrientationBuilder'),
             ),
-            body: MyHomePage()));
+            body: MyHomePage(),
+            floatingActionButton: FloatingActionButton(
+              onPressed: _changeColor,
+              child: Icon(Icons.swap_vert),
+            ),
+          )),
+    );
+  }
+
+  void _changeColor() {
+    setState(() {});
   }
 }
 
@@ -38,9 +73,7 @@ class MyHomePage extends StatelessWidget {
             orientation.toString(),
             style: TextStyle(
               fontSize: 32.0,
-              color: orientation == Orientation.landscape
-                  ? Colors.red
-                  : Colors.blue,
+              color: InheritedColor.of(context).color,
             ),
           );
         },

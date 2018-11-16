952e2d28d5d897934573c113feae17b7aa0603a3
Filip Hracek <filip.hracek@gmail.com>
2018-11-15 16:56:20 -0800

    Form

--- lib/main.dart
+++ lib/main.dart
@@ -5,60 +5,37 @@ void main() {
   runApp(new MyApp());
 }
 
-final someVariable = 0;
-
-final _one = Optional.of(MyClass("one"));
-
-/// When this function gets any simpler, the bug doesn't seem to materialize.
-Optional<MyClass> getOptional() {
-  switch (someVariable) {
-    case 0:
-      return new Optional<MyClass>.absent();
-    case 1:
-      return _one;
-  }
-}
-
-/// The value being wrapped by [Optional] must be a non-primitive value.
-class MyClass {
-  final String text;
-
-  MyClass(this.text);
-}
-
-/// This is `package:quiver`'s `Optional` class, simplified as much
-/// as possible.
-class Optional<T> {
-  final T _value;
-
-  const Optional.absent() : _value = null;
-
-  Optional.of(this._value);
-
-  String toString() {
-    final buf = StringBuffer();
-    buf.writeln("T: $T");
-    buf.writeln("_value: $_value");
-    buf.writeln("_value.runtimeType: ${_value.runtimeType}");
-    buf.writeln("_value == null: ${_value == null}");
-    buf.write(_value == null
-        ? 'Optional { absent }'
-        : 'Optional { non-null value: ${_value} }');
-    return buf.toString();
-  }
-}
-
-// ---- Flutter-specific code
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final optionalInstance = getOptional();
-    final string = optionalInstance.toString();
-
     return MaterialApp(
+      theme: ThemeData(
+          textTheme: TextTheme(
+        body1: TextStyle(fontSize: 18),
+        caption: TextStyle(fontSize: 18),
+        overline: TextStyle(fontSize: 18),
+      )),
       home: Scaffold(
-        body: Center(child: Text(string)),
+        body: SafeArea(
+          minimum: EdgeInsets.all(16.0),
+          child: Center(
+            child: Form(
+              autovalidate: true,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Enter name'),
+                  TextFormField(
+                    validator: (value) {
+                      if (value.isEmpty) return 'Name cannot be blank.';
+                      if (value == 'Filip') return 'Unlikely spelling!';
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }

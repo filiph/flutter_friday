d7b679981d5f73781f13205daae5c3c289e01498
Filip Hracek <filip.hracek@gmail.com>
2019-02-25 14:57:10 -0800

    SizedBox episode

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,32 +11,54 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
           textTheme: TextTheme(
+        button: TextStyle(fontSize: 18, color: Colors.white),
         body1: TextStyle(fontSize: 18),
         caption: TextStyle(fontSize: 18),
         overline: TextStyle(fontSize: 18),
       )),
       home: Scaffold(
+        appBar: AppBar(
+          title: Text('SizedBox Sample'),
+        ),
         body: SafeArea(
           minimum: EdgeInsets.all(16.0),
           child: Center(
-            child: Form(
-              autovalidate: true,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Enter name'),
-                  TextFormField(
-                    validator: (value) {
-                      if (value.isEmpty) return 'Name cannot be blank.';
-                      if (value == 'Filip') return 'Unlikely spelling!';
-                    },
-                  ),
-                ],
-              ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MyButton(),
+                SizedBox(height: 200),
+                MyOtherButton(),
+              ],
             ),
           ),
         ),
       ),
     );
   }
+}
+
+class MyButton extends StatelessWidget {
+  final String text = 'Button';
+
+  const MyButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      onPressed: () {},
+      child: Text(
+        text,
+        style: TextStyle(color: Colors.white),
+      ),
+      color: Colors.blue,
+    );
+  }
+}
+
+class MyOtherButton extends MyButton {
+  @override
+  final String text = 'Other Button';
 }
